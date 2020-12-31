bbde4e8cad3eb4fbadecb6aeba3c3b589a3a332f
Paul Taylor <pltorigami@gmail.com>
2020-12-31 12:35:46 -0500

    updated requirements.txt

--- lorem.py
+++ lorem.py
@@ -1,6 +1,8 @@
-import sys
 import random
 import string
+import sys
+
+"""Generate random text to be used as hidden messages in images"""
 
 
 def makeSentOrPar(oldList, sentence=True):
@@ -14,26 +16,26 @@ def makeSentOrPar(oldList, sentence=True):
     sentParLength = random.randint(lowerBound, upperBound)
     returnList = []
     counter = 0
-    sentPar = ''
+    sentPar = ""
 
     for wordSent in oldList:
         if counter == 0:
             if sentence:
-                sentPar += wordSent.title() + ' '
+                sentPar += wordSent.title() + " "
             else:
-                sentPar += '\t' + wordSent + ' '
+                sentPar += "\t" + wordSent + " "
             counter += 1
         elif counter == sentParLength:
             if sentence:
-                sentPar += wordSent + '.'
+                sentPar += wordSent + "."
             else:
-                sentPar += wordSent + '\n'
+                sentPar += wordSent + "\n"
             returnList.append(sentPar)
-            sentPar = ''
+            sentPar = ""
             sentParLength = random.randint(lowerBound, upperBound)
             counter = 0
         else:
-            sentPar += wordSent + ' '
+            sentPar += wordSent + " "
             counter += 1
 
     returnList.append(sentPar.rstrip())
@@ -44,22 +46,26 @@ def makeSentOrPar(oldList, sentence=True):
 def lorem(number):
     wordsList = []
     for _ in range(number):
-        wordsList.append(''.join([
-            random.choice(string.ascii_lowercase)
-            for _ in range(random.randint(1, 7))
-        ]))
+        wordsList.append(
+            "".join(
+                [
+                    random.choice(string.ascii_lowercase)
+                    for _ in range(random.randint(1, 7))
+                ]
+            )
+        )
 
     sentenceList = makeSentOrPar(wordsList)
     paragraphList = makeSentOrPar(sentenceList, sentence=False)
 
-    return ''.join(paragraphList).rstrip()
+    return "".join(paragraphList).rstrip()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         numWords = int(sys.argv[1])
         print(lorem(numWords))
     except ValueError:
-        print('ERROR: positional argument must be an integer')
+        print("ERROR: positional argument must be an integer")
     except IndexError:
-        print('ERROR: must include a positional argument for number of words.')
+        print("ERROR: must include a positional argument for number of words.")
